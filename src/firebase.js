f5fc847056b49dfdcc47eb2be65f4ae941f1b4bd
Shawal Kabir Chy <shawalkabirchy2020@gmail.com>
2020-10-25 14:00:28 +0600

    Changed

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl7Cw1lkD8pzYglLtA11vThF_0LEWCMo0",
-  authDomain: "discord-clone-kabir.firebaseapp.com",
-  databaseURL: "https://discord-clone-kabir.firebaseio.com",
-  projectId: "discord-clone-kabir",
-  storageBucket: "discord-clone-kabir.appspot.com",
-  messagingSenderId: "791000113054",
-  appId: "1:791000113054:web:9c3784916fd06fabe0008f",
-  measurementId: "G-QGCC5G6K23"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -17,5 +17,5 @@ const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export {auth,provider};
+export { auth, provider };
 export default db;
